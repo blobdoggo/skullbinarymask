4c3b6deaf6e4974e7508b06245d5916d65cffd93
Rushil Mathur <rushilmathur2003@gmail.com>
2025-05-29 14:31:56 -0400

    Changed tensorflow to torch

--- checkgpu.py
+++ checkgpu.py
@@ -1,5 +1,3 @@
-import tensorflow as tf
-
-print("TensorFlow version:", tf.__version__)
-print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
-print("GPU Devices:", tf.config.list_physical_devices('GPU'))
+import torch
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name(0))
